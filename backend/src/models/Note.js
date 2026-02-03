4f633ebd0f6c5cc6e500705e81bc25263b4ed863
JCSheeron <jeffsheeron@gmail.com>
2026-02-02 17:23:18 -0800

    Fixed schema structure regarding timestamps

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -1,17 +1,21 @@
 import mongoose from 'mongoose';
 
 // create a schema for a note
-const noteSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
   },
-  content: {
-    type: String,
-    required: true,
+  {
+    timestamps: true, // createdAt, updatedAt
   },
-  timestamps: true, // createdAt, updatedAt
-});
+);
 
 // create model from schema
 const Note = mongoose.model('Note', noteSchema);
