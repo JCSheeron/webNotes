32062a075d44a41f921679361945fc97ed0906f7
JCSheeron <jeffsheeron@gmail.com>
2026-02-02 17:25:17 -0800

    Begin to tie in db

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,27 +1,32 @@
+import Note from '../models/Note.js';
+
 // Get notes function
-export function getAllNotes(req, res) {
-  // get notes
-  res
-    .status(200)
-    .send(
-      "You have retreived 15 notes via a router and a funciton. Busted a move.",
-    );
+export async function getAllNotes(req, res) {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error('Error in getAllNotes controller', error);
+    res
+      .status(500)
+      .json({ message: 'Internal Server Error during getAllNotes.' });
+  }
 }
 
 // Create a note function
 export function createNote(req, res) {
   // Create a note
-  res.status(201).json({ message: "Note created sucessfully!" });
+  res.status(201).json({ message: 'Note created successfully!' });
 }
 
 // Update a note function
 export function updateNote(req, res) {
   // Update a note by id
-  res.status(200).json({ message: "Note updated sucessfully!" });
+  res.status(200).json({ message: 'Note updated successfully!' });
 }
 
 // Delete a note function
 export function deleteNote(req, res) {
   // Delete a note by id
-  res.status(200).json({ message: "Note deleted sucessfully!" });
+  res.status(200).json({ message: 'Note deleted successfully!' });
 }
