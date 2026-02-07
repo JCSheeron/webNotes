9efe106145ca103646ac682c8c1bc0df342ea3ca
JCSheeron <jeffsheeron@gmail.com>
2026-02-06 20:07:17 -0800

    Added tailwindcss v4. Allows css in html.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,12 @@ import toast from "react-hot-toast";
 const App = () => {
   return (
     <div>
-      <button onClick={() => toast.success("Congrats!!!")}>Click Me!</button>
+      <button
+        className="text-red-500 p-4"
+        onClick={() => toast.success("Congrats!!!")}
+      >
+        Click Me!
+      </button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
