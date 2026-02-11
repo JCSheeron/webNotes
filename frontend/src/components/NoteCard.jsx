3725b9cade9e949f0d780ba2db64d983166eeb14
JCSheeron <jeffsheeron@gmail.com>
2026-02-10 19:55:45 -0800

    Got delete working with immediate UI refresh

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,10 +1,32 @@
 import { Link } from "react-router";
 import { formatDate } from "../lib/utils";
+import axiosApi from "../lib/axios";
+import toast from "react-hot-toast";
 // icons
-import { PenSquareIcon, Trash2Icon } from "lucide-react";
+import {
+  AppleIcon,
+  PenSquareIcon,
+  TableRowsSplit,
+  Trash2Icon,
+} from "lucide-react";
 // import Note from "../../../backend/src/models/Note";
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+  const handleDelete = async (e, id) => {
+    e.preventDefault(); // don't want navigation behavior of NoteCard Link
+
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+
+    try {
+      await axiosApi.delete(`/notes/${id}`);
+      setNotes((prev) => prev.filter((note) => note._id !== id)); // get rid of note id just deleted
+      toast.success("Note deleted successfully");
+    } catch (error) {
+      console.log("Error in handleDelete", error);
+      toast.error("Failed to delete note");
+    }
+  };
+
   return (
     <Link
       to={`/note/${note._id}`}
@@ -19,7 +41,10 @@ const NoteCard = ({ note }) => {
           </span>
           <div className="flex items-center gap-1">
             <PenSquareIcon className="size-4" />
-            <button className="btn btn-ghost btn-xs text-error">
+            <button
+              className="btn btn-ghost btn-xs text-error"
+              onClick={(e) => handleDelete(e, note._id)}
+            >
               <Trash2Icon className="size-4" />
             </button>
           </div>
